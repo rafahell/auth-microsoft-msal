49cf1fc351c9a5daf3032376f8da19eef457f5bb
Rafael <220rrs@Gmail.com>
2020-05-13 04:48:51 -0700

    change ignore file

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+
+namespace GraphClient
+{
+    public class Program{
+        private const string _clientId = "818d0aae-a33f-44fb-957a-54340164d22d";
+        private const string _tenantId = "206f248c-2a21-47cf-9a5e-e7c8a3c537d5";
+        public static async Task Main(string[] args){
+            IPublicClientApplication app;
+            app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("http://localhost")
+                .Build();
+
+            List<string> scopes = new List<string> { 
+                "user.read" 
+            };
+
+            // AuthenticationResult result;
+            // result = await app
+            // .AcquireTokenInteractive(scopes)
+            // .ExecuteAsync();
+            // Console.WriteLine($"Token:\t{result.AccessToken}");
+            DeviceCodeProvider provider = new DeviceCodeProvider(app, scopes);
+            GraphServiceClient client = new GraphServiceClient(provider);
+
+            User myProfile = await client.Me
+            .Request()
+            .GetAsync();
+
+            Console.WriteLine($"Name:\t{myProfile.DisplayName}");
+            Console.WriteLine($"AAD Id:\t{myProfile.Id}");
+        }
+        
+    }
+}
